9ad0445bbfb260dc530ff2d2418d7f472db19f24
Dave Larson <dlarson@genome.wustl.edu>
2016-11-29 08:51:53 -0600

    skeleton of tests for Alignment class

---
+++ test/lib/process_splitters/TestAlignment.cpp
@@ -0,0 +1,42 @@
+#include "process_splitters/Alignment.hpp"
+
+#include <sam.h>
+
+#include <gtest/gtest.h>
+
+TEST(TestAlignment, create_from_bam) {
+
+}
+
+TEST(TestAlignment, create_from_sa) {
+
+}
+
+TEST(TestAlignment, calculate_additional_offsets) {
+
+}
+
+TEST(TestAlignment, start_diagonal) {
+
+}
+
+TEST(TestAlignment, end_diagonal) {
+
+}
+
+TEST(TestAlignment, mno) {
+
+}
+
+TEST(TestAlignment, desert) {
+
+}
+
+TEST(TestAlignment, insert_size) {
+
+}
+
+TEST(TestAlignment, should_check) {
+
+}
+
