9042f3f235cb20742413b45d16dc48b194488409
Dave Larson <dlarson@genome.wustl.edu>
2016-12-01 22:09:22 -0600

    more tests

--- test/lib/process_splitters/TestSamReader.cpp
+++ test/lib/process_splitters/TestSamReader.cpp
@@ -4,12 +4,21 @@
 #include <sam.h>
 
 #include <gtest/gtest.h>
+#include <stdexcept>
 
 TEST(TestSamReader, create) {
     SamReader reader(TEST_BAMS[0].path.c_str());
     bam1_t* record = bam_init1();
 
-    EXPECT_TRUE(reader.next(record));
-    EXPECT_TRUE(reader.next(record));
-    EXPECT_FALSE(reader.next(record));
+    ASSERT_TRUE(reader.next(record));
+    ASSERT_TRUE(reader.next(record));
+    ASSERT_FALSE(reader.next(record));
+}
+
+TEST(TestSamReader, failed_create) {
+    ASSERT_THROW(SamReader reader("nonexistent_file"), std::runtime_error);
+}
+
+TEST(TestSamReader, failed_reference) {
+    ASSERT_THROW(SamReader reader(TEST_BAMS[1].path.c_str(), "nonexistent_reference"), std::runtime_error);
 }
