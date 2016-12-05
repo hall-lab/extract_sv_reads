ff2d481e6d37fb397391697f023b57f101293582
Dave Larson <dlarson@genome.wustl.edu>
2016-12-05 15:54:44 -0600

    add functor to remove sequence and quality values

---
+++ src/lib/extract_sv_reads/BamTransform.hpp
@@ -0,0 +1,54 @@
+#pragma once
+
+#include <sam.h>
+#include <cstddef>
+#include <string.h>
+#include <kseq.h>
+#include <stdint.h>
+
+class BamTransformer {
+    private:
+        bam1_t* _aln;
+        std::size_t _pre_seq_bytes(bam1_t const* b) const {
+            return (b->core.n_cigar<<2) + b->core.l_qname;
+        }
+
+        std::size_t _seq_qual_bytes(bam1_t const* b) const {
+            // Total - pre-sequence bytes - sequence bytes - quality bytes
+            return (((b->core.l_qseq + 1)>>1) + b->core.l_qseq);
+        }
+
+        std::size_t _post_qual_bytes(bam1_t const* b) const {
+            // Total - pre-sequence bytes - sequence bytes - quality bytes
+            return (b->l_data - _pre_seq_bytes(b) - _seq_qual_bytes(b));
+        }
+
+    public:
+        BamTransformer()
+            : _aln(bam_init1())
+        {}
+
+        ~BamTransformer() {
+            bam_destroy1(_aln);
+        }
+
+        bam1_t* operator()(bam1_t const* b) {
+            // Largely based off of htslib's bam_copy1 function
+            uint8_t* data = _aln->data;
+            int m_data = _aln->m_data;
+            if (m_data < b->l_data) {
+                m_data = b->l_data; kroundup32(m_data);
+                data = (uint8_t*) realloc(data, m_data);
+            }
+            // NOTE Copying everything BUT sequence and quality
+            memcpy(data, b->data, _pre_seq_bytes(b));
+            memcpy(data + _pre_seq_bytes(b), bam_get_aux(b), _post_qual_bytes(b));
+            *_aln = *b;
+            _aln->l_data = b->l_data - _seq_qual_bytes(b);
+            _aln->core.l_qseq = 0;
+            _aln->m_data = m_data;
+            _aln->data = data;
+
+            return _aln;
+        }
+};
---
+++ test/lib/process_splitters/TestBamTransform.cpp
@@ -0,0 +1,29 @@
+#include "extract_sv_reads/BamTransform.hpp"
+#include "TestData.hpp"
+#include "testing/TestBamRecords.hpp"
+
+#include <sam.h>
+
+#include <gtest/gtest.h>
+
+class TestBamTransform : public ::testing::Test {
+    public:
+        bam1_t *record1;
+        bam1_t *record2;
+        TestBamRecords records;
+
+        TestBamTransform() {
+            record1 = records.record;
+            record2 = records.record2;
+        }
+};
+
+TEST_F(TestBamTransform, transform) {
+    BamTransformer transformer;
+    bam1_t* transformed = transformer(record1);
+    ASSERT_EQ(0, transformed->core.l_qseq);
+    ASSERT_EQ(record1->l_data - 152, transformed->l_data);
+    ASSERT_EQ(*bam_get_aux(record1), *bam_get_aux(transformed));
+    ASSERT_EQ(*(record1->data + record1->l_data - 1), *(transformed->data + transformed->l_data - 1));
+}
+
